559232d8d2db1bb47243bb3a68a09023fc182e31
Hállan da Silva Costa <hallan.costa1@hotmail.com>
2021-03-01 19:58:53 -0300

    chore(mobile): change baseURL to ipv4
    Signed-off-by: Hállan da Silva Costa <hallan.costa1@hotmail.com>

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://api-animesonline-cc.herokuapp.com'
+  baseURL: 'http://192.168.0.9:3333'
+  // baseURL: 'https://api-animesonline-cc.herokuapp.com'
 });
